0518bfcb7ef792acfa2c64011ee8d99d5f01897e
koma5 <marco@5th.ch>
2012-02-03 16:56:37 +0100

    for testing...

---
+++ api/post.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  
+  <meta charset="utf-8">
+  <title>post to blauerHimmel api</title>
+  
+  </head>
+  
+  <body>
+
+<form action="index.php" method="post">
+	<textarea class="xxlarge" id="data" name="data" rows="20">
+﻿{
+	"name":"drerscht",
+	"apikey":"4cdqfTqhL4SswTTeA4sOL4SswTTeA4sO",
+	"point":
+		[
+			{
+				"lat":"4707.4282",
+				"long":"00853.7112",
+				"speed":"0",
+				"time":"212225",
+				"date":"250112"
+				
+			},
+			
+			{
+				"lat":"4807.4283",
+				"long":"00953.7112",
+				"speed":"0",
+				"time":"212226",
+				"date":"250112"
+			}
+
+		]
+}	
+
+
+	</textarea>
+	<button type="submit" name="action" value="save">post data</button>
+</form>
+
+  
+  </body>
+
+</html>
+
