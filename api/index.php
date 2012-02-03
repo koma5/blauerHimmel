0860f96bd06b65fe5ed74bf49c543fc459959416
koma5 <marco@5th.ch>
2012-02-03 17:55:58 +0100

    complex error handling / but it's working ;)

--- api/index.php
+++ api/index.php
@@ -6,48 +6,67 @@ include 'lib/db.class.php';
 $myDB = new Database();
 $myDB->connect();
 
+//initialize error code
+$error = 1; //everything OK
 
 //get data and decode
-$data = $_REQUEST['data'];
-		/*$handle = fopen('php://input','r');
-		$jsonInput = fgets($handle);
-		$data = json_decode($jsonInput,true);*/
+if (isset($_REQUEST['data']))$data = $_REQUEST['data'];
+else $error = 0; //if there's no data
 
-
-$dataObj = json_decode(prepareJSON($data));
-json_last_error();
-//#dev echo $data;
-//#dev var_dump($dataObj);
-//#dev echo "data recieved from: " . $dataObj->name . "[" . $dataObj->apikey . "]<br>";
-
-
-// auth
-$result = mysql_query("
-SELECT COUNT(*) FROM `tReceiver`
-WHERE recName = '".$dataObj->name."' AND
-recApiKey = '".$dataObj->apikey."'
-");
-if ($result) $recCount = mysql_result($result, 0);
-if ($recCount == 1) $auth = 1;
-else
+if ($error != 0)
 {
-	$auth = 0;
-	$error = 2;
-}
 
+	$dataObj = json_decode(prepareJSON($data));
+	/*$handle = fopen('php://input','r');
+	$jsonInput = fgets($handle);
+	$data = json_decode($jsonInput,true);*/
+	//#dev json_last_error();
+	//#dev echo $data;
+	//#dev 
+	var_dump($dataObj);
+	//#dev echo "data recieved from: " . $dataObj->name . "[" . $dataObj->apikey . "]<br>";
+
+
+	// auth ######## do it with selecting the ID you will need it at the insert ;)
+	$result = mysql_query("
+	SELECT COUNT(*) FROM `tReceiver`
+	WHERE recName = '".$dataObj->name."' AND
+	recApiKey = '".$dataObj->apikey."'
+	");
+	if ($result) $recCount = mysql_result($result, 0);
+	if ($recCount == 1) $auth = 1;
+	else
+	{
+		$auth = 0;
+		$error = 2;
+	}
+
+	if($auth != 0)
+	{
+		foreach ($dataObj->point as $point)
+		{
+			$inputQuery = "INSERT INTO blahhhhh";
+			echo $inputQuery;
+		}
+
+
+	}
+
+
+}
 
 switch ($error) {
     case 1:
         echo '{"error":["code":"1","name":"data saved ... everything OK"]}';
         break;
     case 0:
-        echo '{"error":["code":"0","name":"some error"]}';
+        echo '{"error":["code":"0","name":"no data received"]}';
         break;
     case 2:
         echo '{"error":["code":"2","name":"authentication failed! go away!"]}';
         break;
     case 3:
-        echo '{"error":["code":"3","name":"no data received"]}';
+        echo '{"error":["code":"3","name":""]}';
         break;
 }
 
