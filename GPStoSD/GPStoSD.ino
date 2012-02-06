afa9f4ef670730a6639d267e1ddba4ffd41cd05d
Stektograph <St_steiner@hotmail.com>
2012-02-06 23:49:37 +0100

    Update GPStoSD/GPStoSD.ino

--- GPStoSD/GPStoSD.ino
+++ GPStoSD/GPStoSD.ino
@@ -1,110 +1,152 @@
+//-------------------------------------------------------------------------------------------------------------------------------
+// File     :  GpsToSd.c
+// Name     :  Stefan Steiner & Marco Koch
+// Date     :  06.02.2012
+// Platform :  Arduino
+// Function :  Get Data from the GPS modul pmb-248 and save them to a SD-Card
+//-------------------------------------------------------------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------------------------------------------------------------
+//Includes
+//-------------------------------------------------------------------------------------------------------------------------------
 #include <SoftwareSerial.h>
 #include <SD.h>
 #include <String.h>
+#include <avr/pgmspace.h>
+//-------------------------------------------------------------------------------------------------------------------------------
 
-//SoftwareSerial(RX,TX)
-SoftwareSerial GPS = SoftwareSerial(2,3);
+//-------------------------------------------------------------------------------------------------------------------------------
+//Hardware Variables 
+//-------------------------------------------------------------------------------------------------------------------------------
+SoftwareSerial GPS = SoftwareSerial(2,3); //SoftwareSerial(RX,TX)
+const int chipSelect = 4;                 //
+const int StatusLED = 12;                 //
+//-------------------------------------------------------------------------------------------------------------------------------
 
-//var declaration
-const int chipSelect = 4;
-const int StatusLED = 12;
+//-------------------------------------------------------------------------------------------------------------------------------
+//Flags
+//-------------------------------------------------------------------------------------------------------------------------------
+boolean GPSrd = true;    //is GPS receive data
+boolean NMEAstr = false;  //NMEA string received
+boolean usData = false;   //are usefull Datas available
+//-------------------------------------------------------------------------------------------------------------------------------
+//Global Variables
+//-------------------------------------------------------------------------------------------------------------------------------
+int NMEAlevel = 0; //showes active level of array, active means the one who will be filled now... works as FILO
+char NMEA[8][100] //can save 8 NMEA sentence 
 
-int countNMEA = 0;
-char NMEA[100] = "";
-String parsedNMEA;
 
+//-------------------------------------------------------------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------------------------------------------------------------
+//setup
+//-------------------------------------------------------------------------------------------------------------------------------
 void setup()
 {
-  //serial data configuration for pmb-248
-  GPS.begin(4800); //baudrate: 4800 (fixed)
-  //serial data configuration for computer
-  Serial.begin(9600); //baudrate: 9600
   
-  pinMode(StatusLED, OUTPUT);
+  GPS.begin(4800); //serial data configuration for pmb-248, baudrate 4800
+  Serial.begin(9600); //serial data configuration for computer, baudrate: 9600
+  
+  pinMode(StatusLED, OUTPUT); //
 
-/*
-  if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
-    return;
-    
-  Serial.println("card initialized.");
-  }*/
+  for(int y = 0; y<8;y++)                       //empty NMEA[][]
+     for(int x = 0; x<100;x++)                  //
+		 NMEA[y][x] = ' ';                      //
+
 
 }
+//-------------------------------------------------------------------------------------------------------------------------------
 
-
+//-------------------------------------------------------------------------------------------------------------------------------
+//loop
+//-------------------------------------------------------------------------------------------------------------------------------
 void loop()
-{
+{ 
+  if ((GPS.available()) && (GPSrd == true)) //GPS data are available and are allowed to receive
+  {
+		  getGPSData();
+  }
+
+  if(NMEAstr == true) //NMEA String received -> Parse
+  {
+	  parseNMEA(); 
+	  NMEAstr == false;   //wait for next NMEA string
+  }
   
-  //File logFile = SD.open("gps.txt", FILE_WRITE);
-  digitalWrite(StatusLED, HIGH);
-  getGPSData();
-  digitalWrite(StatusLED, LOW);
-    /*
-    logFile.println(dataChar);
-    logFile.close();
-    // print to the serial port too:
-
-    Serial.write(dataChar);*/
-    
+  if(usData == true) //usefull Data ready to send to SD
+  {
+	  //Send Data
+	  usData = false; //wait for next Data
+  }
 }
+//-------------------------------------------------------------------------------------------------------------------------------
 
+//-------------------------------------------------------------------------------------------------------------------------------
+//Function     : void getGPSData(void);
+//task         : get an NMEA string 
+//Parameter    : void
+//Return       : void
+//-------------------------------------------------------------------------------------------------------------------------------
 void getGPSData()
-{
-    
-    if (GPS.available()){
-   
-        NMEA[countNMEA] = GPS.read();
-        /* // #dev print out every char
-        Serial.print("[");
-        Serial.print(countNMEA, DEC);
-        Serial.print("]:");
-        Serial.print(NMEA[countNMEA]);*/
+{   
+	int countNMEA = 0;
+
+	while(GPS.available()) //get all sended data not just one char
+	{
+        NMEA[NMEAlevel][countNMEA] = GPS.read(); //Get one char
         
-        if (NMEA[countNMEA] == '$')
+        if (NMEA[NMEAlevel][countNMEA] == '$') //if a dollarsign is received -> end of NMEA sentence
         {
-           //if a dollarsign is received -> end of NMEA sentence
-           //Serial.println("data receivd");  
-           NMEA[countNMEA] = '\0'; //replace dollarsign with end of string
+           NMEA[NMEAlevel][countNMEA] = '\0';  //replace dollarsign with end of string
+            
+           NMEAstr = true; //NMEAstr received set flag
+ 
+		   if(NMEAlevel >8) //no Overflow
+			   NMEAlevel++; //change to next NMEA level. next string can be saved
+  
+          for(int x = 0; x<100;x++)NMEA[NMEAlevel][x] = ' '; //empty new NMEA level
           
-           //for(int x = 0; NMEA[x] != '\0';x++)Serial.print(NMEA[x]);
-          
-          parseNMEA(NMEA);
-    
-          for(int y = 0; y<100;y++)NMEA[y] = ' '; //empty NMEA[]
-          
-          NMEA[0] = '$';
-          countNMEA = 0;
+          NMEA[NMEAlevel][0] = '$'; //new String beginns with $
+          countNMEA = 0;            
         }
+		
+        countNMEA++;                 
+	}
+	NMEA[NMEAlevel][countNMEA+1] = '\0'; //to avoid endless String
 
-    countNMEA++; 
-  
-    }
 }
+//-------------------------------------------------------------------------------------------------------------------------------
 
-void parseNMEA(const char input[])
+//-------------------------------------------------------------------------------------------------------------------------------
+//Function     : void parseNMEA(void)
+//task         : parse an NMEA string and save data to a struct
+//Parameter    : void
+//Return       : void
+//-------------------------------------------------------------------------------------------------------------------------------
+void parseNMEA()
 {
-  parsedNMEA = input;
-  //for(int x = 0; input[x] != '\0';x++)Serial.print(input[x]);
-  //Serial.print(parsedNMEA);
-  
-  
-  if(parsedNMEA.indexOf("$GPRMC") >= 0)
-  {
-    //if 
-      Serial.print("found $GPRMC at:");
-      Serial.print(parsedNMEA.indexOf("$GPRMC"));
-      Serial.print(parsedNMEA);
-  }
+   
+   while(NMEAlevel >=0)//get last saved string and check every string in the NMEA array
+   {
+	   int NMEAstring = "";
+	   NMEAstring = NMEA[NMEAlevel]; //copy string
+	   NMEAlevel--;
 
-  
-  if(parsedNMEA.indexOf("$GPGGA") >= 0)
-  {
-    //if 
-      Serial.print("found $GPGGA at:");
-      Serial.print(parsedNMEA.indexOf("$GPGGA"));
-      Serial.print(parsedNMEA);
-      
-  }
+	   if(parsedNMEA.indexOf("$GPRMC") >= 0) 	//check string
+	   {
+		  Serial.print("found $GPRMC at:");
+          Serial.print(parsedNMEA);
+		  usData = true;  //usefull data found
+
+
+
+	   }
+       if(parsedNMEA.indexOf("$GPGGA") >= 0)
+	   {
+	      Serial.print("found $GPGGA at:");
+          Serial.print(parsedNMEA);
+		  usData = true; //usefull data found
+	   }
+   }
 }
+
