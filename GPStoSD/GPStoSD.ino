dcdad406b5eacb2669c284d9806e99f3071cf8cb
koma5 <marco@5th.ch>
2012-02-11 15:31:56 +0100

    debugging

--- GPStoSD/GPStoSD.ino
+++ GPStoSD/GPStoSD.ino
@@ -12,7 +12,7 @@
 #include <SoftwareSerial.h>
 #include <SD.h>
 #include <String.h>
-#include <avr/pgmspace.h>
+//#include <avr/pgmspace.h>
 //-------------------------------------------------------------------------------------------------------------------------------
 
 //-------------------------------------------------------------------------------------------------------------------------------
@@ -35,13 +35,16 @@ boolean usData = false;   //are usefull Datas available
 int NMEAlevel = 0; //showes active level of array, active means the one who will be filled now... works as FILO
 char NMEA[8][100]; //can save 8 NMEA sentence 
 
-    //specific data from GPS receiver
-	int time; //reicht int abklären
-	float breitengrad;
-	float laengengrad;
-	float geschwindigkeit;
-	int datum;
-	int hoehe;
+int NMEAcharcount = 0; //count 
+
+
+                           //in string -> we send string for calculating
+	String time;              //long  //
+	String latitude;          //float //
+	String longitude;         //float //
+	String Speed;             //float // in knot -> 1 knot = 1,852 km/h
+	String  date;             //int   // day, month, year
+	String  altitude;         //int   // above sea level
 
 //-------------------------------------------------------------------------------------------------------------------------------
 
@@ -58,7 +61,7 @@ void setup()
 
   for(int y = 0; y<8;y++)                       //empty NMEA[][]
      for(int x = 0; x<100;x++)                  //
-		 NMEA[y][x] = ' ';                      //
+		 NMEA[y][x] = 'x';                      //
 
 
 }
@@ -72,19 +75,29 @@ void loop()
   if ((GPS.available()) && (GPSrd == true)) //GPS data are available and are allowed to receive
   {
 		  getGPSData();
+                // for(int x = 0; x<8;x++)Serial.print(NMEA[x]);  
+
   }
 
+
+                 
   if(NMEAstr == true) //NMEA String received -> Parse
   {
-	  parseNMEA(); 
+	  parseNMEA();
 	  NMEAstr == false;   //wait for next NMEA string
   }
   
-  if(usData == true) //usefull Data ready to send to SD
+/*  if(usData == true) //usefull Data ready to save to SD
   {
-	  //Send Data
+          Serial.print(time);
+	  Serial.print(latitude);
+	  Serial.print(longitude);
+	  Serial.print(Speed);
+	  Serial.print(date);
+	  Serial.print(altitude); 
+          //save Data
 	  usData = false; //wait for next Data
-  }
+  }*/
 }
 //-------------------------------------------------------------------------------------------------------------------------------
 
@@ -96,30 +109,32 @@ void loop()
 //-------------------------------------------------------------------------------------------------------------------------------
 void getGPSData()
 {   
-	int countNMEA = 0;
-
 	while(GPS.available()) //get all sended data not just one char
 	{
-        NMEA[NMEAlevel][countNMEA] = GPS.read(); //Get one char
+        NMEA[NMEAlevel][NMEAcharcount] = GPS.read(); //Get one char
+        //Serial.print(NMEA[NMEAlevel][NMEAcharcount]);
         
-        if (NMEA[NMEAlevel][countNMEA] == '$') //if a dollarsign is received -> end of NMEA sentence
+        if (NMEA[NMEAlevel][NMEAcharcount] == '$') //if a dollarsign is received -> end of NMEA sentence
         {
-           NMEA[NMEAlevel][countNMEA] = '\0';  //replace dollarsign with end of string
+           NMEA[NMEAlevel][NMEAcharcount] = '\0';  //replace dollarsign with end of string
             
            NMEAstr = true; //NMEAstr received set flag
  
-		   if(NMEAlevel >8) //no Overflow
+           Serial.print(NMEAlevel);
+           Serial.print(NMEA[NMEAlevel]);
+		   if(NMEAlevel <7) //no Overflow
 			   NMEAlevel++; //change to next NMEA level. next string can be saved
+
   
-          for(int x = 0; x<100;x++)NMEA[NMEAlevel][x] = ' '; //empty new NMEA level
+          for(int x = 0; x<100;x++)NMEA[NMEAlevel][x] = ' '; //prepare for filling new NMEA sentence
           
           NMEA[NMEAlevel][0] = '$'; //new String beginns with $
-          countNMEA = 0;            
+          NMEAcharcount = 0;            
         }
-		
-        countNMEA++;                 
+
+        NMEAcharcount++;                 
 	}
-	NMEA[NMEAlevel][countNMEA+1] = '\0'; //to avoid endless String
+	//NMEA[NMEAlevel][NMEAcharcount+1] = '\0'; //to avoid endless String
 
 }
 //-------------------------------------------------------------------------------------------------------------------------------
@@ -132,29 +147,77 @@ void getGPSData()
 //-------------------------------------------------------------------------------------------------------------------------------
 void parseNMEA()
 {
-   
+   Serial.print("Begin parse");
+   Serial.print(NMEAlevel);
+              Serial.print(NMEA[NMEAlevel]);
+
    while(NMEAlevel >=0)//get last saved string and check every string in the NMEA array
    {
-	   int NMEAstring = "";
-	   NMEAstring = NMEA[NMEAlevel]; //copy string
+	   String NMEAstring = NMEA[NMEAlevel];//copy string
 	   NMEAlevel--;
 
-	   if(parsedNMEA.indexOf("$GPRMC") >= 0) 	//check string
+	   if(NMEAstring.indexOf("$GPRMC") !=-1) 	//check string
 	   {
-		  Serial.print("found $GPRMC at:");
-          Serial.print(parsedNMEA);
-		  usData = true;  //usefull data found
-
-
-
-
+		   if(getdatastr(NMEAstring, 2).equals("A"))//valid data
+		   {
+			/*  time = getdatastr(NMEAstring, 1);              
+			  latitude = getdatastr(NMEAstring, 3);          
+			  longitude = getdatastr(NMEAstring, 4);     
+			  Speed = getdatastr(NMEAstring, 5);            
+			  date = getdatastr(NMEAstring, 7);      
+*/
+                          usData = true;  //usefull data 
+                  }
+		  
 	   }
-       if(parsedNMEA.indexOf("$GPGGA") >= 0)
+
+       if(NMEAstring.indexOf("$GPGGA") != -1)
 	   {
-	      Serial.print("found $GPGGA at:");
-          Serial.print(parsedNMEA);
+          //altitude = getdatastr(NMEAstring, 7);      
 		  usData = true; //usefull data found
 	   }
    }
+   NMEAlevel = 0;
+   NMEAstr = false;
+      Serial.print("end parse");
+
 }
 
+
+//-------------------------------------------------------------------------------------------------------------------------------
+//Function     : String getdatastr(String NMEAsente, int wdata)
+//task         : receive a string and which data want to find out and return this data in another string
+//Parameter    : StringNMEAsente : string which includes a NMEA sentence
+//               int wdata       :  which data you want to find out, begins with one (in $GPRMC string, that's the time)     
+//Return       : String          :  returns a string with the data
+//                                  or 0 in an errorcase
+//-------------------------------------------------------------------------------------------------------------------------------
+String getdatastr(String NMEAsente, int wdata)
+{
+	int startplace = 0;
+	int stopplace =0;
+    
+	startplace = NMEAsente.indexOf(',');//index of first ','
+	
+	if(startplace == -1) return 0; // errorcase no comma in the string
+
+    while(wdata > 1) //find out first place of first string
+	{
+		startplace = NMEAsente.indexOf(',', startplace);
+		if(startplace== -1) return 0;
+	}
+	startplace +=1; //place on first data not at comma
+
+	stopplace = NMEAsente.indexOf(',', startplace); //end of data
+	
+	if(stopplace ==-1) //last data of NMEA sentence is searched
+	{
+		return NMEAsente.substring(startplace);
+	}
+	else
+	{
+		return NMEAsente.substring(startplace, stopplace);
+	}
+
+
+}
