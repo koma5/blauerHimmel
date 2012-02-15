3cd1236fc82246995bab916ea9c6b39922eb714e
marco <marco@5th.ch>
2012-02-15 18:29:11 +0100

    added dummy data from Costa Concordia

--- sql/dummy_data.sql
+++ sql/dummy_data.sql
@@ -1,5 +1,6 @@
 INSERT INTO `tReceiver` (`recName`,`recApiKey`) VALUES
-('drerscht','4cdqfTqhL4SswTTeA4sOL4SswTTeA4sO')
+('drerscht','4cdqfTqhL4SswTTeA4sOL4SswTTeA4sO'),
+('concordia', 'concordia')
 ;
 
 
