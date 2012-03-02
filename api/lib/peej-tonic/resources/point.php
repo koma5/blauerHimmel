cdedc3f53c890b5b35e285993a2a1d3fb0409586
marco <marco@5th.ch>
2012-03-02 20:39:12 +0100

    new awesome php objects to generate json / I love json

--- api/lib/peej-tonic/resources/point.php
+++ api/lib/peej-tonic/resources/point.php
@@ -10,10 +10,31 @@ class PointResource extends Resource {
 
 	function get($request, $receiver) {
 
+		$myReceiverPointsResponse = (object) array(
+		'receiver' => (object) array(
+			'name' => $receiver,
+			'points' => array()
+			)
+		);
+
+		$myPoint = (object) array(
+			'lat' => 4807.4283,
+			'long' => 953.7112,
+			'speed' => 0,
+			'date' => "2012-01-25 21:22:25"
+		);
+
+		$i =0;
+		while ($i < 3)
+		{
+			array_push($myReceiverPointsResponse->receiver->points, $myPoint);
+			$i++;
+		}
+
 		$response = new Response($request);
 		$response->code = Response::OK;
 		$response->addHeader('content-type', 'text/json');
-		$response->body = '{"receiver":"'.$receiver.'","points":[{"lat":4807.4283,"long":953.7112,"speed":0,"date":"2012-01-25 21:22:25"}}]}';
+		$response->body = json_encode($myReceiverPointsResponse);
 		return $response;
 
 	}
--- api/lib/peej-tonic/resources/receiver.php
+++ api/lib/peej-tonic/resources/receiver.php
@@ -10,11 +10,18 @@ class ReceiverResource extends Resource {
 
 	function get($request, $receiver) {
 
+		$myReceiverResponse = (object) array (
+		'receiver' => (object) array (
+			'name' => $receiver,
+			'lastupdate' => '2012-02-15 18:15:50',
+			'count' => 518
+			)
+		);
+
 		$response = new Response($request);
 		$response->code = Response::OK;
-		$response->addHeader('content-type', 'text/json');
-		$response->body = 
-		'{"receiver":{"name":"'.$receiver.'","lastupdate": "2012-02-15 18:15:50","count": 518}}';
+		$response->addHeader('content-type', 'application/json');
+		$response->body = json_encode($myReceiverResponse);
 		return $response;
 
 	}
