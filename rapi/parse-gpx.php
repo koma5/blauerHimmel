b12145a8ecfdc41fea3ace825d402b4f1be44903
marco <marco@5th.ch>
2012-06-28 21:45:21 +0200

    gpx-file parser... generating rapi post for easy copy and pasting into simulator post.php

---
+++ rapi/parse-gpx.php
@@ -0,0 +1,37 @@
+
+<?php
+
+
+$response = (object) array(
+	'name' => 'toeffli',
+	'apikey' => 'slow',
+	'point' => array()
+);
+
+$xml = simplexml_load_file('2012-06-27-siebnen-vorderthal.gpx');
+echo $xml->getName() . "<br/>";
+
+$points = $xml->trk->trkseg->trkpt;
+
+foreach ($points as $p) {
+
+	$attributes = $p->attributes();
+
+	$date = preg_replace(array('/T/', '/Z/'), array(' ',''), $p->time);
+
+	$myPoint = (object) array(
+		'lat' => (float) $attributes->lat,
+		'long' => (float) $attributes->lon,
+		'speed' => 0,
+		'att' => (float) $p->ele,
+		'date' => (string) $date
+	);
+
+	array_push($response->point, $myPoint);
+
+}
+
+echo json_encode($response);
+
+
+?>
